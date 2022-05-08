e90b176578118b55e6bc9ec1db0abed56e3911d6
AlexMancinas <alexman381@gmail.com>
2022-05-08 15:56:49 -0700

    sql server conexion

---
+++ ConexionSQLSERVER/ConexionSQLSERVER/Conexion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace ConexionSQLSERVER
+{
+    internal class Conexion
+    {
+        public static SqlConnection Conexionsql()
+        {
+            SqlConnection connection = new SqlConnection("Data Source=.;Initial Catalog=Ejemplo1;Integrated Security=True");
+            connection.Open();
+            return connection;
+        }
+    }
+}
---
+++ ConexionSQLSERVER/ConexionSQLSERVER/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using ConexionSQLSERVER;
+namespace ConexionSQLSERVER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Conexion.Conexionsql();
+            MessageBox.Show("Conexion exitosa");
+
+            dataGridView1.DataSource = Consulta();
+        }
+        public DataTable Consulta()
+        {
+            Conexion.Conexionsql();
+            DataTable datos = new DataTable();
+            string consultar = "SELECT * FROM Alumnos";
+            SqlCommand cmd = new SqlCommand(consultar, Conexion.Conexionsql());
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(datos);
+            return datos;
+
+        }
+
+    }
+}
